d401a50254e7f6fb69d535995a9c74292c570d88
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-02-13 18:49:05 +0000

    add sign in and sign out, local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Container from "react-bootstrap/Container"
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import HomeScreen from "./Screens/HomeScreen"
@@ -8,38 +10,62 @@ import Nav from "react-bootstrap/Nav"
 import { LinkContainer } from "react-router-bootstrap"
 import Badge from "react-bootstrap/esm/Badge"
 import { useContext } from "react"
-import {Store} from './Store'
+import { Store } from "./Store"
 import CartScreen from "./Screens/CartScreen"
 import SigninScreen from "./Screens/SigninScreen"
+import NavDropdown from "react-bootstrap/NavDropdown"
 
 function App() {
-    const {state} = useContext(Store)
-    const {cart} = state
+    const { state, dispatch: ctxDispatch } = useContext(Store)
+    const { cart, userInfo } = state
+    const signoutHandler = () => {
+        ctxDispatch({ type: "USER_SIGNOUT" })
+        localStorage.removeItem("userInfo")
+    }
     return (
         <BrowserRouter>
             <div className="d-flex flex-column site-container">
+                <ToastContainer position = 'bottom-center' limit={1}/>
                 <header>
                     <Navbar bg="dark" variant="dark">
                         <Container>
                             <LinkContainer to="/">
                                 <Navbar.Brand>amazona</Navbar.Brand>
                             </LinkContainer>
-                            <Nav className='me-auto'>
-                                <Link to='/cart' className="nav-link">
+                            <Nav className="me-auto">
+                                <Link to="/cart" className="nav-link">
                                     Cart
-                                    {cart.cartItems.length>0&&(
-                                        <Badge pill bg='danger'>
-                                            {cart.cartItems.reduce((a,c)=>a+c.quantity,0)}
+                                    {cart.cartItems.length > 0 && (
+                                        <Badge pill bg="danger">
+                                            {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                                         </Badge>
                                     )}
                                 </Link>
+                                {userInfo ? (
+                                    <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                                        <LinkContainer to="/profile">
+                                            <NavDropdown.Item>User Profile</NavDropdown.Item>
+                                        </LinkContainer>
+                                        <LinkContainer to="/orderhistory">
+                                            <NavDropdown.Item>Order History</NavDropdown.Item>
+                                        </LinkContainer>
+                                        <NavDropdown.Divider />
+                                        <Link className="dropdown-item" to="#signout" onClick={signoutHandler}>
+                                            Sign Out
+                                        </Link>
+                                    </NavDropdown>
+                                ) : (
+                                    <Link className="nav-link" to="/signin">
+                                        Sign In
+                                    </Link>
+                                )}
                             </Nav>
                         </Container>
                     </Navbar>
                 </header>
 
                 <main>
-                    <Container  className="mt-3">
+                    <Container className="mt-3">
                         <Routes>
                             <Route path="/" element={<HomeScreen />} />
                             <Route path="/cart" element={<CartScreen />} />
